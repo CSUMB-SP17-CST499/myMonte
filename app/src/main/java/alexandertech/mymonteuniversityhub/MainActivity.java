8e809b3ce7885fb7759da2a4ac09220776244e78
Javar Alexander <Jaalexander@csumb.edu>
2017-02-24 13:49:52 -0800

    adding UPD and Study room feature

--- app/src/main/java/alexandertech/mymonteuniversityhub/MainActivity.java
+++ app/src/main/java/alexandertech/mymonteuniversityhub/MainActivity.java
@@ -1,17 +1,13 @@
 package alexandertech.mymonteuniversityhub;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
