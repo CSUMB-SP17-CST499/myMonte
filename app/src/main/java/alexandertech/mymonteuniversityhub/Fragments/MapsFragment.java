70ba0f999f6d13a9bb152259da655f295e5cd7c3
Joseph Molina <jomolina@csumb.edu>
2017-03-30 20:31:22 -0700

    Maps checks if GPS is on

--- app/src/main/java/alexandertech/mymonteuniversityhub/Fragments/MapsFragment.java
+++ app/src/main/java/alexandertech/mymonteuniversityhub/Fragments/MapsFragment.java
@@ -1,19 +1,19 @@
 package alexandertech.mymonteuniversityhub.Fragments;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,30 +21,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationAvailability;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -61,7 +59,6 @@ import alexandertech.mymonteuniversityhub.R;
 
 import static alexandertech.mymonteuniversityhub.Activities.MainActivity.prefs;
 import static alexandertech.mymonteuniversityhub.Activities.MainActivity.sharedPrefs;
-import static android.content.Context.MODE_PRIVATE;
 
 
 public class MapsFragment extends Fragment implements
@@ -74,14 +71,14 @@ public class MapsFragment extends Fragment implements
     private GoogleMap mGoogleMap;
     private GoogleApiClient mGoogleApiClient;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
     private Location mLastLocation;
     private MapView mMapView;
     Button saveParkingLocation;
     Button lastParkingLocation;
     LatLng currentParkingLocation;
     String serverKey = "AIzaSyCSNFes9NZKUDrslyRG8Z-9qQ9X37df1Os";
-    Polyline line;
-    private ArrayList<LatLng> locationList;
+    private static final int REQUEST_CHECK_SETTINGS = 2;
 
 
     @Override
@@ -97,6 +94,8 @@ public class MapsFragment extends Fragment implements
 
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
+        Log.d("OnCreate", "Inside onCreate");
+
 
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
@@ -105,8 +104,6 @@ public class MapsFragment extends Fragment implements
                     .addApi(LocationServices.API)
                     .build();
         }
-
-        //checkLocationPermission();
         return view;
     }
 
@@ -115,23 +112,24 @@ public class MapsFragment extends Fragment implements
     public void onMapReady(GoogleMap googleMap) {
         Log.d("MapsFragment", "Inside onMapReady");
         mGoogleMap = googleMap;
-        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        LatLng csumb = new LatLng(36.6524204, -121.79731630000003);
-        mGoogleMap.addMarker(new MarkerOptions().position(csumb).title("Marker in CSUMB"));
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(csumb, 18));
-        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
     }
 
     private void setUpMap() {
+        Log.v("setUpMap", "Inside setUpMap");
+
         if (ActivityCompat.checkSelfPermission(getActivity(),
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     LOCATION_PERMISSION_REQUEST_CODE);
             return;
         }
+
         //Enables the blue dot and gps button that when tapped centers the map on user location.
         mGoogleMap.setMyLocationEnabled(true);
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+
         //Determining the availability of location data on the device.
         LocationAvailability locationAvailability = LocationServices.FusedLocationApi.getLocationAvailability(mGoogleApiClient);
         if (null != locationAvailability && locationAvailability.isLocationAvailable()) {
@@ -141,32 +139,50 @@ public class MapsFragment extends Fragment implements
             if (mLastLocation != null) {
                 LatLng currentLocation = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
                 currentParkingLocation = currentLocation;
-                // locationList.add(0,currentLocation);//adding current location in arraylist
-                //placeMarkerOnMap(currentLocation);
                 mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 18));
             }
         }
     }
 
-    protected void placeMarkerOnMap(LatLng location) {
-        //Creates obj and sets the users current location as the position for marker.
-        MarkerOptions markerOptions = new MarkerOptions().position(location);
-        mGoogleMap.addMarker(markerOptions);
-    }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.saveLocationButton:
                 if (currentParkingLocation == null) {
-                    Toast.makeText(getActivity(), "Turn on GPS and press GPS button to set your location", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(),
+                            "Turn on GPS and press GPS button to set your location",
+                            Toast.LENGTH_SHORT).show();
+                    settingsRequest();
                 } else {
-                    Toast.makeText(getActivity(), currentParkingLocation.toString(), Toast.LENGTH_SHORT).show();
-                    String longitude = String.valueOf(currentParkingLocation.longitude);
-                    String latitude = String.valueOf(currentParkingLocation.latitude);
-                    prefs.putString("location", latitude + "," + longitude);
-                    prefs.apply();
-                    Toast.makeText(getActivity(), "Saved!", Toast.LENGTH_SHORT).show();
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Toast.makeText(getActivity(), currentParkingLocation.toString(),
+                                    Toast.LENGTH_SHORT).show();
+                            String longitude = String.valueOf(currentParkingLocation.longitude);
+                            String latitude = String.valueOf(currentParkingLocation.latitude);
+                            prefs.putString("location", latitude + "," + longitude);
+                            prefs.apply();
+                            Toast.makeText(getActivity(), "Parking Location Saved!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(getActivity(), "Canceled saving the parking location",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                    builder.setMessage(R.string.dialog_message)
+                            .setTitle(R.string.dialog_title);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
                 }
 
                 break;
@@ -178,7 +194,8 @@ public class MapsFragment extends Fragment implements
                     String[] locationArray = sharedPrefs.getString("location", "").split(",");
                     Double retrievedLat = Double.parseDouble(locationArray[0]);
                     Double retrievedLong = Double.parseDouble(locationArray[1]);
-                    Toast.makeText(getActivity(), retrievedLat + " and " + retrievedLong, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), retrievedLat + " and "
+                            + retrievedLong, Toast.LENGTH_SHORT).show();
 
                     LatLng latlng = new LatLng(retrievedLat, retrievedLong);
                     // locationList.add(1,latlng);//adding parking location
@@ -193,75 +210,6 @@ public class MapsFragment extends Fragment implements
                     Toast.makeText(getActivity(), "No saved location", Toast.LENGTH_SHORT).show();
                 }
         }
-    }
-
-    /*private void build_retrofit_and_get_response(String type){
-        String url = "https://maps.googleapis.com/maps/";
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(url)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        RetrofitMaps service = retrofit.create(RetrofitMaps.class);
-        Call<Example> call = service.getDistanceDuration("metric", mLastLocation.getLatitude() + "," + mLastLocation.getLongitude(), currentParkingLocation.latitude + "," + currentParkingLocation.longitude, type);
-        call.enqueue(new Callback<Example>() {
-            @Override
-            public void onResponse(Response<Example> response, Retrofit retrofit) {
-                for(int i = 0; i < response.body().getRoutes().size();i++){
-                    //String distance = response.body().getRoutes().get(i).getLegs().get(i).getDistance().getText();
-                   // String time = response.body().getRoutes().get(i).getLegs().get(i).getDuration().getText();
-                    //ShowDistanceDuration.setText("Distance:" + distance + ", Duration:" + time);
-                    String encodedString = response.body().getRoutes().get(0).getOverviewPolyline().getPoints();
-                    List<LatLng> list = decodePoly(encodedString);
-                    line = mGoogleMap.addPolyline(new PolylineOptions()
-                            .addAll(list)
-                            .width(20)
-                            .color(Color.RED)
-                            .geodesic(true)
-                    );
-                }
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-
-            }
-        });
-
-
-    }*/
-
-    private List<LatLng> decodePoly(String encoded) {
-        List<LatLng> poly = new ArrayList<LatLng>();
-        int index = 0, len = encoded.length();
-        int lat = 0, lng = 0;
-
-        while (index < len) {
-            int b, shift = 0, result = 0;
-            do {
-                b = encoded.charAt(index++) - 63;
-                result |= (b & 0x1f) << shift;
-                shift += 5;
-            } while (b >= 0x20);
-            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-            lat += dlat;
-
-            shift = 0;
-            result = 0;
-            do {
-                b = encoded.charAt(index++) - 63;
-                result |= (b & 0x1f) << shift;
-                shift += 5;
-            } while (b >= 0x20);
-            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-            lng += dlng;
-
-            LatLng p = new LatLng((((double) lat / 1E5)),
-                    (((double) lng / 1E5)));
-            poly.add(p);
-        }
-
-        return poly;
     }
 
     @Override
@@ -283,8 +231,67 @@ public class MapsFragment extends Fragment implements
     @Override
     public void onStart() {
         super.onStart();
-
         mGoogleApiClient.connect();
+        settingsRequest();
+    }
+
+    public void settingsRequest(){
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        final PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(@NonNull LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates state = result.getLocationSettingsStates();
+                switch (status.getStatusCode()){
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        setUpMap();
+                        // All location settings are satisfied. The client can initialize location
+                        // requests here.
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(getActivity(), REQUEST_CHECK_SETTINGS);
+                            setUpMap();
+                        }catch (IntentSender.SendIntentException e){
+                            //ignore this error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way to fix the
+                        // settings so we won't show the dialog.
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getActivity(), "OK!!!!",Toast.LENGTH_SHORT).show();
+                        //startLocationUpdates();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        //settingsrequest();//keep asking if imp or do whatever
+                        break;
+                }
+                break;
+        }
     }
 
     @Override
@@ -305,7 +312,7 @@ public class MapsFragment extends Fragment implements
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        setUpMap();
+       // setUpMap();
 
     }
 
@@ -416,6 +423,38 @@ public class MapsFragment extends Fragment implements
             parserTask.execute(result);
 
         }
+    }
+    private List<LatLng> decodePoly(String encoded) {
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+
+        return poly;
     }
 
     /**
