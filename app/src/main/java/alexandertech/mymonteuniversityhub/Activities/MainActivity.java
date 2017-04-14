62dcd3640d61f601d0ebcd0f6dc28bc48f823121
Javar Alexander <Jaalexander@csumb.edu>
2017-04-14 13:58:03 -0700

    after merge w / migration there was an issue on main activity. fixed issue, now commiting changes.

--- app/src/main/java/alexandertech/mymonteuniversityhub/Activities/MainActivity.java
+++ app/src/main/java/alexandertech/mymonteuniversityhub/Activities/MainActivity.java
@@ -153,7 +153,6 @@ public class MainActivity extends AppCompatActivity
             fragments[position] = createdFragment;
             return createdFragment;
         }
-
     }
     /*
     Method for the navigation Drawer that takes in the id from the navigation drawer
